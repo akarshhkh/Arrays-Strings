a6b824a5f4860ef96af6c3b32b7977b9269c1f14
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:29:15 +0530

    Create printing_array.cpp

---
+++ printing_array.cpp
@@ -0,0 +1,22 @@
+//akarsh khot
+//23070123012
+//Aim - to print all the numbers present in the array.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n =5;
+    int arr[n] = {10 , 20 , 30 , 40 , 50}; 
+    for (int i = 0; i < n ; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+    return 0;
+}
+
+
+
+/*
+OUTPUT
+10 20 30 40 50
+*/
