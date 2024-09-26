efc20bc3ff78828b5de386be9ed2d19e79d93966
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:28:01 +0530

    Create concatenation.cpp

---
+++ concatenation.cpp
@@ -0,0 +1,25 @@
+/*
+akarsh khot
+23070123012
+Aim - to print the concatenation of the following string i.e "sit pune"
+*/
+
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+    
+    string firstname = "sit"; 
+    string lastname = "pune";
+    string fullname = firstname + lastname; 
+    
+    cout << fullname << endl; 
+    return 0;
+}
+
+/*
+OUTPUT
+sitpune
+*/
